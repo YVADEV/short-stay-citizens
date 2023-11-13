28a1dd5a0fa338f4e20625496ecd43ecdc444afd
MitreaRazvan <48449356+MitreaRazvan@users.noreply.github.com>
2023-11-13 12:49:03 +0200

    new updates v.1.0

--- src/script/script.js
+++ src/script/script.js
@@ -27,16 +27,70 @@ requestAnimationFrame(raf);
       );
     });
 
+    //swiper
 
-    const slidesContainer = document.querySelector(".slides-container");
-    const slideWidth = slidesContainer.querySelector(".slide").clientWidth;
-    const prevButton = document.querySelector(".prev");
-    const nextButton = document.querySelector(".next");
-  
-    nextButton.addEventListener("click", () => {
-      slidesContainer.scrollLeft += slideWidth;
-    });
-  
-    prevButton.addEventListener("click", () => {
-      slidesContainer.scrollLeft -= slideWidth;
-    });
+    var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      initialSlide: 1,
+      loop: true,
+      slidesPerView: 'auto',
+      spaceBetween: 30, // Adjust space between slides
+      coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 20,
+          modifier: 1,
+          slideShadows: true,
+          centeredSlides: true,
+
+  // Set slidesPerView to a fraction on mobile to show part of the next slide
+  slidesPerView: 1.1,
+  spaceBetween: 10, // adjust the space to create the "peek" effect
+
+      },
+      
+      // Removed pagination and navigation initialization
+      breakpoints: {
+          // when window width is >= 320px
+          320: {
+              slidesPerView: 1.5,
+              spaceBetween: 20,
+              coverflowEffect: {
+                  rotate: 0,
+                  stretch: 0,
+                  depth: 50,
+                  modifier: 1,
+                  slideShadows: false,
+              },
+          },
+          // when window width is >= 640px
+          640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+              coverflowEffect: {
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+              },
+          },
+          // when window width is >= 1024px
+          1024: {
+              slidesPerView: 3,
+              spaceBetween: 30,
+              coverflowEffect: {
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 100,
+                  modifier: 1,
+                  slideShadows: true,
+              },
+          },
+      }
+  });
+
+
+//no scroll
